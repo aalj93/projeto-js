6321988de999ca00037c561266e31ce9c73fee7d
Antonio Junior <juniorjp93@hotmail.com>
2024-09-19 17:31:21 -0300

    arq. projeto js

---
+++ script.js
@@ -0,0 +1,8 @@
+function verificar(){
+   var data = new Date()
+   var ano = data.getFullYear()
+   var fano = document.getElementById('txtano')
+   var res = document.querySelector('div#res')
+   if (fano.value.length == 0 || fano.vale > ano) {window.alert('[erro] essa data não está correspondendo!')
+   }
+}
